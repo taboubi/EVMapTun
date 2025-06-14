e7271432589b739ffe817d26eedb41d581f710e9
Issam Taboubi <issam.taboubi@umontreal.ca>
2025-06-14 18:59:56 -0400

    add script env logs - v6

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,15 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ev.charge.car',
+  appId: 'ev.chargemap.tun',
   appName: 'ev-charge-car',
-  webDir: 'www'
+  webDir: 'www',
+   plugins: {
+    AdMob: {
+      appIdAndroid: 'ca-app-pub-7963319750770607~6481253029',
+      appIdIos: 'ca-app-pub-7963319750770607~2681075984'
+    }
+  }
 };
 
 export default config;
--- src/app/services/ads.service.ts
+++ src/app/services/ads.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { AdMob } from '@capacitor-community/admob';
+import { Platform } from '@ionic/angular';
 
 @Injectable({ providedIn: 'root' })
 export class AdsService {
-  private interstitialId = environment.admobInterstitialId;
+  constructor(private platform: Platform) {}
+
+  private getInterstitialId(): string {
+    if (this.platform.is('android')) {
+      return environment.admobInterstitialIdAndroid;
+    } else if (this.platform.is('ios')) {
+      return environment.admobInterstitialIdIos;
+    }
+    return '';
+  }
 
   async showInterstitialAd(): Promise<void> {
+    const adId = this.getInterstitialId();
     try {
       await AdMob.prepareInterstitial({
-        adId: this.interstitialId,
+        adId,
         isTesting: true,
       });
       await AdMob.showInterstitial();
