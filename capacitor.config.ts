279832c096b1d1a99ca2d46e6176f872e39c23d1
Issam Taboubi <issam.taboubi@umontreal.ca>
2025-06-14 19:10:36 -0400

    add script env logs - v7

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,18 @@ const config: CapacitorConfig = {
   appId: 'ev.chargemap.tun',
   appName: 'ev-charge-car',
   webDir: 'www',
-   plugins: {
+  plugins: {
     AdMob: {
       appIdAndroid: 'ca-app-pub-7963319750770607~6481253029',
       appIdIos: 'ca-app-pub-7963319750770607~2681075984'
+    }
+  },
+  cordova: {
+    preferences: {
+      'android.permission.ACCESS_FINE_LOCATION': 'true',
+      'android.permission.ACCESS_COARSE_LOCATION': 'true',
+      'NSLocationWhenInUseUsageDescription': 'Cette application a besoin de votre position pour afficher les stations à proximité.',
+      'NSLocationAlwaysUsageDescription': 'Cette application a besoin d’un accès permanent à votre position pour certaines fonctionnalités.'
     }
   }
 };
