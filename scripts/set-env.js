6a4d7f781d3690d17c2a86e53cfbe98e0e02c595
Issam Taboubi <issam.taboubi@umontreal.ca>
2025-06-14 17:23:51 -0400

    add script env logs - v5

--- scripts/set-env.js
+++ scripts/set-env.js
@@ -1,42 +1,19 @@
 const fs = require('fs');
 const path = require('path');
 
-console.log('--- [set-env.js] Démarrage du script ---');
+const envContent = process.env.ENV_FILE_CONTENT;
 
-const apiKey = process.env.apiKey;
-const authDomain = process.env.authDomain;
-const projectId = process.env.projectId;
-const storageBucket = process.env.storageBucket;
-const messagingSenderId = process.env.messagingSenderId;
-const appId = process.env.appId;
-const googleMapsApiKey = process.env.googleMapsApiKey;
-const measurementId = process.env.measurementId;
-const admobInterstitialId = process.env.admobInterstitialId;
-
-try {
-  const filePath = path.join(__dirname, '../src/environments/environment.ts');
-
-  const content = `
-export const environment = {
-  production: true,
-  firebase: {
-    apiKey: '${apiKey}',
-    authDomain: '${authDomain}',
-    projectId: '${projectId}',
-    storageBucket: '${storageBucket}',
-    messagingSenderId: '${messagingSenderId}',
-    appId: '${appId}',
-    measurementId: '${measurementId}'
-  },
-  googleMapsApiKey: '${googleMapsApiKey}',
-  admobInterstitialId: '${admobInterstitialId}'
-};
-`;
-
-  fs.writeFileSync(filePath, content);
-  console.log('✅ environment.ts généré avec succès', content);
-  console.log('✅ environment.ts généré avec succès');
-} catch (err) {
-  console.error('❌ Erreur dans set-env.js:', err);
+if (!envContent) {
+  console.error("❌ ENV_FILE_CONTENT n'est pas défini !");
   process.exit(1);
 }
+
+const filePath = path.join(__dirname, '../src/environments/environment.ts');
+const filePathprod = path.join(__dirname, '../src/environments/environment.prod.ts');
+
+// Astuce : remplace les caractères d'échappement si nécessaire
+const decodedContent = envContent.replace(/\\n/g, '\n');
+
+fs.writeFileSync(filePath, decodedContent);
+fs.writeFileSync(filePathprod, decodedContent);
+console.log("✅ Fichier environment.ts mis à jour à partir de ENV_FILE_CONTENT");
